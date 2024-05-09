eaacec3797da764ec0f1cad736200ae5516bc91c
Mauro Leggieri <mxmauro@gmail.com>
2024-05-09 10:59:38 -0300

    Increased version and nicer log output

--- src/tests/simple.test.ts
+++ src/tests/simple.test.ts
@@ -1,28 +1,29 @@
 import test, { ExecutionContext } from 'ava';
 import { load } from '..';
 import { errMsg, testVars } from './helpers';
+import { KeyValueSet } from '../types';
 
 // -----------------------------------------------------------------------------
 
 test('Process Environment variables', async (t: ExecutionContext) => {
-	try {
-		if (typeof process.env['VAULT_URL'] === 'string') {
-			t.log('VAULT_URL found! Tests will try to connect to Vault in order to get variables.');
-		}
-		else {
-			t.log('VAULT_URL not found. Only process environment variables will be processed.');
-		}
+	let values: KeyValueSet;
 
-		const values = await load({
+	if (typeof process.env['VAULT_URL'] !== 'string') {
+		t.fail('VAULT_URL not found. Only process environment variables will be processed.');
+	}
+	t.log('VAULT_URL found! Tests will try to connect to Vault in order to get variables.');
+
+	try {
+		values = await load({
 			vars: testVars
 		});
-
-		for (const [ k, v ] of Object.entries(values)) {
-			t.log(k, '=', v);
-		}
 	}
 	catch (err: any) {
 		t.fail(errMsg(err));
+	}
+
+	for (const [ k, v ] of Object.entries(values)) {
+		t.log(k, '=', v);
 	}
 	t.pass();
 });
